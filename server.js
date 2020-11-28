9fb2b9ed32cf8272a224c05573f54ba55d4f81c1
rtirabassi1994 <tirabassi.rachel@gmail.com>
2020-11-28 14:33:53 -0500

    server2

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-//serve static content
+
 app.use(express.static("public"));
 
-//Parse application body 
+//Parse the app body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//set Handlebars
+//handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -21,7 +21,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-//Start server so that it can listen
+//Start server
 app.listen(PORT, function() {
     console.log(`Server listening on: http://localhost:${PORT}`);
 });
