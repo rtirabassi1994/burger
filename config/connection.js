a53e889e3acb7f56ebf7e88326d03ec00f61cf5d
rtirabassi1994 <tirabassi.rachel@gmail.com>
2021-02-07 13:27:09 -0500

    connection fixes

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 // var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   var connection = mysql.createConnection({
     host: "localhost",
